e597f8c5c9a54fcd82970b4eacb529691348aefc
krzysztofKoszela <42913047+krzysztofKoszela@users.noreply.github.com>
2021-06-22 19:34:41 +0200

    Update dbcinfofetch.sh

--- dbcinfofetch.sh
+++ dbcinfofetch.sh
@@ -13,22 +13,3 @@ LOG_FILEPATH="${LOG_DIR}/dbcinfofetch.log"
 
 bteq < ${SCRIPT_PATH} > ${LOG_FILEPATH}
 
- 
-
-ReturnCode=$?
-
- 
-
-if[[ ${ReturnCode} -eq 0 ]]; then
-
-                 echo "DBC info FETCH script completed successfully"
-
-                 exit 0
-
-else
-
-                 echo "DBC info FETCH script failled"
-
-                 exit 1
-
-fi
